302b56e1060c3628c622a5adab9501dbdea0f36b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-10-29 09:42:38 +0900

    リファクタリング

--- tools/resume.sh
+++ tools/resume.sh
@@ -4,7 +4,7 @@ script_dir=$(dirname  "$0")
 vagrant_dir="${script_dir}/../vagrant"
 
 pushd "${vagrant_dir}"
-mkdir -p  /cygdrive/w/Vagrant/ubuntu-develop/vagrant
+mkdir -p /cygdrive/w/Vagrant/ubuntu-develop/vagrant
 
 time  vagrant resume ; echo $?
 
