969c8a3403fa0be2674dfd80a1f51cefebc7c7ec
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-03-30 17:55:36 +0900

    追加ハードディスク関連を別ファイルに分割

---
+++ vagrant/AttachNewHdd.rb
@@ -0,0 +1,45 @@
+##  -*-  coding: utf-8; mode: ruby -*-  ##
+##  vi: set ft=ruby
+
+Vagrant.configure("2") do |config|
+
+  config.vm.provider "virtualbox" do |v|
+    #
+    # ディスクを追加する
+    #
+    disk_file = './disk/ubuntu-develop-sdc.vdi'
+    unless File.exists?(disk_file)
+      v.customize [
+        'createmedium',     'disk',
+        '--filename',       disk_file,
+        '--size',           50 * 1024,
+        '--format',         'VDI',
+      ]
+    end
+
+    if ARGV.include?('destroy')
+    else
+      v.customize [
+        'storageattach',    :id,
+        '--storagectl',     'SCSI',
+        '--port',           2,
+        '--device',         0,
+        '--type',           'hdd',
+        '--medium',         disk_file,
+      ]
+    end
+  end
+
+  config.vm.provision("newhdd", type: "shell",
+                      path: "provision.newhdd.sh",
+                      privileged: true)
+  #
+  # 仮想マシンを停止した時に、デタッチしておく
+  #
+  config.trigger.after :halt do |trigger|
+    trigger.run = {inline: "VBoxManage storageattach '#{machine_id}'" +
+      " --storagectl 'SCSI' --port 2 --device 0 --type hdd --medium none"
+    }
+  end
+
+end
