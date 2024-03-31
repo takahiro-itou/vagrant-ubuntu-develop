107c5a28242956b720d46847f4e91ded40679ae9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-03-31 19:48:30 +0900

    移動によるディレクトリ階層の変化に対応

--- tools/99_initialize.sh
+++ tools/99_initialize.sh
@@ -6,7 +6,9 @@ vagrant_dir="${script_dir}/../vagrant"
 # 所定のディレクトリを、仮想マシンへ転送する用意をする
 project_base_dir=$(readlink -f "${script_dir}/..")
 
-env WORKDIR='/cygdrive/w/Vagrant'   \
+env     \
+    PROJECT_BASE_DIR="${project_base_dir}"      \
+    WORKDIR='/cygdrive/w/Vagrant'               \
     /bin/bash -xue "${vagrant_dir}/setup-userdata.sh"   \
     || exit $?
 
